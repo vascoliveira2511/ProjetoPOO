06ce8c76fcb18d5ce578c2b0bdf2981640ef7cea
Vasco Oliveira <vascoliveira2511@gmail.com>
2021-04-22 08:09:43 +0100

    Update Medios.java

--- Jogador/Medios.java
+++ Jogador/Medios.java
@@ -1,3 +1,12 @@
 public class Medios extends Jogador {
 
+    public Medios() {
+        super();
+    }
+
+    public Medios(int tipoJogador, int velocidade, int resistencia, int destreza, int impulsao, int jogoDeCabeca,
+            int remate, int capacidadeDePasse, int equipa) {
+        super(tipoJogador, velocidade, resistencia, destreza, impulsao, jogoDeCabeca, remate, capacidadeDePasse,
+                equipa);
+    }
 }
