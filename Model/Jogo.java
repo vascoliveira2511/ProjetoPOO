22bf0e16d3766469c15a73fcbd15bac223571d68
Miguel Ângelo Ferreira Fernandes <62012969+MiguelFernandes13@users.noreply.github.com>
2021-05-09 15:37:05 +0100

    Update Jogo.java

--- Model/Jogo.java
+++ Model/Jogo.java
@@ -139,7 +139,7 @@ public class Jogo {
         double dif = probs(e1.overallEquipa(), e2.overallEquipa());
         for (int i=0; i < 9; i++){
             double aleatorio = Math.random();
-            situacoesGolos(e1, e2, dif, aleatorio);
+            this.situacoesGolos(e1, e2, dif, aleatorio);
         }
         
     }
