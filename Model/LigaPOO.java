5fa29dd2ffc5bf823334513d0cdc83ed5e5018d6
MiguelFernandes13 <miguelffernandes13@gmail.com>
2021-05-13 18:59:42 +0100

    ligaPOO e remoçao do central

---
+++ Model/LigaPOO.java
@@ -0,0 +1,16 @@
+import java.util.Map;
+import java.util.List;
+
+/**
+ * Write a description of class LigaPOO here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LigaPOO
+{
+    
+    private Map<String, Equipa> equipas;
+    List<Jogo> jogos;
+
+}
