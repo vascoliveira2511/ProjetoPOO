97bc5f1f6589d0d82db267c834de7f146391b476
Vasco Oliveira <vascoliveira2511@gmail.com>
2021-05-14 17:48:09 +0100

    Menu inicial

---
+++ View/Menu.java
@@ -0,0 +1,53 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Menu {
+    private List<String> opcoes;
+    private int op;
+
+    public Menu(String[] opcoes) {
+        this.opcoes = Arrays.asList(opcoes);
+        this.op = 0;
+    }
+
+    public void executa() {
+        do {
+            showMenu();
+            this.op = lerOpcao();
+        } while (this.op == -1);
+    }
+
+    private void showMenu() {
+        System.out.println("\n *** Menu *** ");
+        for (int i = 0; i < this.opcoes.size(); i++) {
+            System.out.print(i + 1);
+            System.out.print(" - ");
+            System.out.println(this.opcoes.get(i));
+        }
+        System.out.println("0 - Sair");
+    }
+
+    private int lerOpcao() {
+        int op;
+        Scanner is = new Scanner(System.in);
+
+        System.out.print("Opção: ");
+        try {
+            op = is.nextInt();
+        } catch (InputMismatchException e) {
+            op = -1;
+        }
+        if (op < 0 || op > this.opcoes.size()) {
+            System.out.println("Opção Invâlida!!!");
+            op = -1;
+        }
+        is.close();
+        return op;
+    }
+
+    public int getOpcao() {
+        return this.op;
+    }
+}
