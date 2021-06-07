1030a1cfb4a2de807ddb8632c4d3b6985750c7d8
MiguelFernandes13 <miguelffernandes13@gmail.com>
2021-06-07 15:58:35 +0100

    sfs

--- Model/Jogo.java
+++ Model/Jogo.java
@@ -215,7 +215,6 @@ public class Jogo implements Serializable {
     public void substituiçoes2()  {
         for (Map.Entry<Integer, Integer> m : this.substituicoesCasa.entrySet()) {
             this.jogadoresCasa.remove(m.getKey());
-            System.out.println(this.equipaCasa.getValue().existeJogador(m.getValue()).toString());
             this.jogadoresCasa.put(m.getValue(), this.equipaCasa.getValue().existeJogador(m.getValue()));
         }
         for (Map.Entry<Integer, Integer> m : this.substituicoesFora.entrySet()) {
