52a62ce329af4fb5b28d7ca350be06b6552f1711
MiguelFernandes13 <miguelffernandes13@gmail.com>
2021-06-08 15:58:47 +0100

    dentro de um ciclo o controlador

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -5,19 +5,24 @@ public class Controller {
     public static void run() throws LinhaIncorretaException, IOException, ClassNotFoundException, EquipaException, JogadorException {
         String[] s = { "Ler Jogo anterior", "Jogar FM-POO EDITION" };
         Menu m = new Menu(s);
-        m.executa();
-        LigaPOO l = null;
-        int op = m.getOpcao();
-        switch (op) {
-            case 1:
-                String r = m.menuLerFicheiro();
-                l = LigaPOO.readFromBinary(r);
-                System.out.println("\nLeitura executada com sucesso!!");
-                ControllerFM.run(l);
-                break;
-            case 2:
-                ControllerFM.run(l);
+        boolean cont = true;
+        while (cont == true) {
+            m.executa();
+            LigaPOO l = null;
+            int op = m.getOpcao();
+            switch (op) {
+                case 1:
+                    String r = m.menuLerFicheiro();
+                    l = LigaPOO.readFromBinary(r);
+                    System.out.println("\nLeitura executada com sucesso!!");
+                    ControllerFM.run(l);
+                    break;
+                case 2:
+                    ControllerFM.run(l);
+                default:
+                    cont = false;
 
+            }
         }
 
     }
--- Controller/ControllerFM.java
+++ Controller/ControllerFM.java
@@ -10,33 +10,38 @@ public class ControllerFM {
         }
         String[] s = {"Simular Jogo", "Consultar Jogador", "Consultar Equipa","Transferir Jogador", "Guardar Jogo"};
         Menu m2 = new Menu(s);
-        m2.executa();
-        int op = m2.getOpcao();
-        switch (op) {
-            case 1:
-                AbstractMap.SimpleEntry<String,String> eqps = m2.menuEscolherEquipas();
-                Equipa e1 = l.existeEquipa(eqps.getKey());
-                Equipa e2 = l.existeEquipa(eqps.getValue());
-                AbstractMap.SimpleEntry<List<Integer>,List<Integer>> eqT = m2.menuEscolheTitulares(e1, e2);
-                AbstractMap.SimpleEntry<Map<Integer, Integer>,Map<Integer, Integer>> subs = m2.menuEscolheSubstituicoes(e1, eqT.getKey(), e2, eqT.getValue());
-                Jogo jogo1 = l.simulaJogo(eqps.getKey(), eqps.getValue(), eqT.getKey(), eqT.getValue(), subs.getKey(), subs.getValue());
-                m2.imprimeJogo(jogo1);
+        boolean cont = true;
+        while (cont == true) {
+            m2.executa();
+            int op = m2.getOpcao();
+            switch (op) {
+                case 1:
+                    AbstractMap.SimpleEntry<String, String> eqps = m2.menuEscolherEquipas();
+                    Equipa e1 = l.existeEquipa(eqps.getKey());
+                    Equipa e2 = l.existeEquipa(eqps.getValue());
+                    AbstractMap.SimpleEntry<List<Integer>, List<Integer>> eqT = m2.menuEscolheTitulares(e1, e2);
+                    AbstractMap.SimpleEntry<Map<Integer, Integer>, Map<Integer, Integer>> subs = m2.menuEscolheSubstituicoes(e1, eqT.getKey(), e2, eqT.getValue());
+                    Jogo jogo1 = l.simulaJogo(eqps.getKey(), eqps.getValue(), eqT.getKey(), eqT.getValue(), subs.getKey(), subs.getValue());
+                    m2.imprimeJogo(jogo1);
 
-            case 2:
-                AbstractMap.SimpleEntry<Integer,String> jog1 = m2.menuEscolherJogador();
-                Jogador j1 = l.procuraJogador(jog1.getValue(), jog1.getKey());
-                m2.imprimeJogador(j1);
-            case 3:
-                Equipa e3 = l.existeEquipa(m2.menuEscolherEquipa());
-                m2.imprimeEquipa(e3);
-            case 4:
-                AbstractMap.SimpleEntry<Integer,AbstractMap.SimpleEntry<String,String>> eqps2 = m2.menuTransferencia();
-                l.transferencia(eqps2.getValue().getKey(), eqps2.getValue().getValue(), eqps2.getKey());
-            case 5:
-                String g = m2.menuGuardarFicheiro();
-                l.saveToBinary(g);
+                case 2:
+                    AbstractMap.SimpleEntry<Integer, String> jog1 = m2.menuEscolherJogador();
+                    Jogador j1 = l.procuraJogador(jog1.getValue(), jog1.getKey());
+                    m2.imprimeJogador(j1);
+                case 3:
+                    Equipa e3 = l.existeEquipa(m2.menuEscolherEquipa());
+                    m2.imprimeEquipa(e3);
+                case 4:
+                    AbstractMap.SimpleEntry<Integer, AbstractMap.SimpleEntry<String, String>> eqps2 = m2.menuTransferencia();
+                    l.transferencia(eqps2.getValue().getKey(), eqps2.getValue().getValue(), eqps2.getKey());
+                case 5:
+                    String g = m2.menuGuardarFicheiro();
+                    l.saveToBinary(g);
+                default:
+                    cont = false;
 
 
+            }
         }
     }
 }
