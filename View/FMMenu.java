f3151dc2bbf0e946d0f424d3362bc45fe98cdc16
MiguelFernandes13 <miguelffernandes13@gmail.com>
2021-06-08 16:10:53 +0100

    erro corrigido e testar so a parte do jogo

--- View/FMMenu.java
+++ View/FMMenu.java
@@ -4,9 +4,15 @@ import java.util.Scanner;
 
 public class FMMenu {
     public static void main(String[] args) throws IOException, InterruptedException, JogadorException {
-        try {
+        /*try {
             Controller.run();
         } catch (LinhaIncorretaException | ClassNotFoundException | EquipaException e) {
+            e.printStackTrace();
+        }*/
+        LigaPOO l = null;
+        try {
+            ControllerFM.run(l);
+        } catch (LinhaIncorretaException | EquipaException e) {
             e.printStackTrace();
         }
         /*Controller c = new Controller(); // Solucao temporaria.
